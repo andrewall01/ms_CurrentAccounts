9d418065673a4f684b0516fd38e0b348d04596b9
Andres Llontop <andresllontop01@gmail.com>
2022-07-14 18:46:55 -0500

    Se actualiza pom

--- src/main/java/com/project/ms/current/accounts/MsCurrentAccountsApplication.java
+++ src/main/java/com/project/ms/current/accounts/MsCurrentAccountsApplication.java
@@ -2,10 +2,12 @@ package com.project.ms.current.accounts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.mongodb.config.EnableReactiveMongoAuditing;
 
 @SpringBootApplication
 @EnableReactiveMongoAuditing
+@EnableEurekaClient
 public class MsCurrentAccountsApplication {
 
 	public static void main(String[] args) {
